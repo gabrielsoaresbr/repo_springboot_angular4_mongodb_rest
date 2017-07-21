b1effd32976e8a7f37b78ce538df7e4fa78bc988
gabriel.soares <gabriel.soares@dell024831.ans.gov.br>
2017-07-21 12:06:15 -0300

    Criando o projeto.

---
+++ restspringangularmongo/src/main/java/restspringangularmongo/repositories/TodoRepository.java
@@ -0,0 +1,10 @@
+package restspringangularmongo.repositories;
+
+import restspringangularmongo.models.Todo;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends MongoRepository<Todo, String> {
+
+}
