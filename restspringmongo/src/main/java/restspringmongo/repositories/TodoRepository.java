748f409f02f2d6dec9ef32b46ee7e073aba22e5e
Gabriel_MeuNote <gabriel.soares.br@gmail.com>
2017-08-03 22:29:52 -0300

    Commit local

---
+++ restspringmongo/src/main/java/restspringmongo/repositories/TodoRepository.java
@@ -0,0 +1,10 @@
+package restspringmongo.repositories;
+
+import restspringmongo.models.Todo;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends MongoRepository<Todo, String> {
+
+}
